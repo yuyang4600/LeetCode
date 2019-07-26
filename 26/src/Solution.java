017fae1bd2971709d2da9310f0ee13698bd9e547
caoxiang <xiang.cao.ext@nokia-sbell.com>
2019-07-26 17:19:27 +0800

    26AC提交

--- 26/src/Solution.java
+++ 26/src/Solution.java
@@ -1,21 +1,18 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        int count = nums.length;
-        for (int i = 0; i < nums.length; i ++) {
-            for (int j = i + 1; j < nums.length; j ++) {
-                if (nums[i] == nums[j]) {
-                    count --;
-                }
+        int number = 0;
+        for (int i=0; i < nums.length ; i++) {
+            if ( nums[i] != nums[number] ) {
+                number++;
+                nums[number] = nums[i];
             }
         }
-        return count;
+        number+=1;
+        return number;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,1,1,1,1,1,2,2,2,2,2,3,3,3,2,2,2,2,2,4,4,4};
+        int[] arr = {1,1,1,2,2,2,3,3,3,4,4,5,6,7,8,9};
         Solution solution = new Solution();
         System.out.println(solution.removeDuplicates(arr));
     }
